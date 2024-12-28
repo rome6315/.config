962983eecf1ad7efe96f1bd555eca626f4c73a16
rome6315 <romo6315@gmail.com>
2024-12-27 21:30:05 -0500

    fix update module

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -1,3 +1,20 @@
 #!/bin/bash 
 
-kitty -e fish -c "paru -Qu; fish"
+format() {
+	if [ "$1" -eq 0 ]; then
+		echo '-'
+	else
+		echo "$1"
+	fi
+}
+
+updates_arch=$(checkupdates 2>/dev/null | wc -l)
+updates_aur=$(paru -Qum 2>/dev/null | wc -l)
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+	echo "($(format "$updates_arch")/$(format "$updates_aur"))"
+else
+	echo "Up to date."
+fi
+
