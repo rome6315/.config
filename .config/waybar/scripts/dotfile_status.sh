406493bbfe05b4971e97698f67638ce874143dc8
rome6315 <romo6315@gmail.com>
2024-12-29 17:08:30 -0500

    add waybar git module

--- .config/waybar/scripts/dotfile_status.sh
+++ .config/waybar/scripts/dotfile_status.sh
@@ -6,19 +6,29 @@ REPO_PATH="$HOME/.config"
 # Get the current branch
 branch=$(git --git-dir=$REPO_PATH symbolic-ref --short HEAD 2>/dev/null)
 
+# ignore these files since all they do is change colors. no need to constantly push color changes
+IGNORE_FILES='waybar/style.css|cava/config|dunst/dunstrc|hypr/scripts/set_wallpaper.sh|hypr/theme.conf|kitty/current-theme.conf|kitty/kitty.conf|kitty/kitty.conf.bak|nwg-dock-hyprland/theme.css|rofi/config.rasi|starship/starship.toml'
+
 # Check for staged, and unstaged changes
 staged=$(git --git-dir=$REPO_PATH diff --cached --numstat | wc -l)
-unstaged=$(git --git-dir=$REPO_PATH --work-tree "$HOME" diff --numstat | wc -l)
+
+# Run the git diff command and exclude the files using the variable
+unstaged=$(git --git-dir=$REPO_PATH --work-tree "$HOME" diff --numstat | \
+  grep -v -E "$IGNORE_FILES" | wc -l)
 
 # Build the status string
 status=""
+
 if [[ $staged -gt 0 ]]; then
-  status+=" $staged staged changes"
+  # If there are unstaged changes, add them first with a space
+  if [[ $unstaged -gt 0 ]]; then
+    status+="  $unstaged unstaged changes"  # spacing so the symbol doesn't collide with the word "changes" from staged
+  fi
+
+  # Now add the staged changes
+  status+="  $staged staged changes"
 fi
 
-if [[ $unstaged -gt 0 ]]; then
-  status+=" $unstaged unstaged changes"
-fi
 
 # Default message if everything is clean
 if [[ -z $status ]]; then
