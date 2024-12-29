30d11ccdeecd89dc57145d4e937704a9b691c4b6
rome6315 <romo6315@gmail.com>
2024-12-28 23:19:01 -0500

    update waybar theming logic

--- .config/scripts/waybar_theme.sh
+++ .config/scripts/waybar_theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "@import 'themes/$1.css';" > $HOME/.config/waybar/theme.css # copy the theme to theme.css which is used to read colors into style.css
+echo "@import 'themes/$1.css';" > $HOME/.config/waybar/style.css # copy the theme into style.css
 
 # reload waybar
 killall waybar
