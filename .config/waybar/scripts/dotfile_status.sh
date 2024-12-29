2c00ad3e954be0d4c37b774b8f2742a7e0475a58
rome6315 <romo6315@gmail.com>
2024-12-29 16:16:29 -0500

    add waybar git module

--- .config/waybar/scripts/dotfile_status.sh
+++ .config/waybar/scripts/dotfile_status.sh
@@ -13,17 +13,17 @@ unstaged=$(git --git-dir=$REPO_PATH --work-tree "$HOME" diff --numstat | wc -l)
 # Build the status string
 status=""
 if [[ $staged -gt 0 ]]; then
-  status+=" $staged staged "
+  status+=" $staged staged"
 fi
 
 if [[ $unstaged -gt 0 ]]; then
-  status+=" $unstaged unstaged "
+  status+=" $unstaged unstaged"
 fi
 
 # Default message if everything is clean
 if [[ -z $status ]]; then
-  status=" Clean"
+  status=" Clean"
 fi
 
 # Output for Waybar
-echo "{\"text\": \"$status\", \"tooltip\": \"Git Repository: $REPO_PATH Branch: $branch\"}"
+echo "{\"text\": \"$status changes\", \"tooltip\": \"Git Repository: $REPO_PATH - Branch: $branch\"}"
