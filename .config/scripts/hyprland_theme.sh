dac07374c93bb19c8b8a75e2404dd5e86195c5d7
rome6315 <romo6315@gmail.com>
2024-12-29 20:17:51 -0500

    update hypr config

--- .config/scripts/hyprland_theme.sh
+++ .config/scripts/hyprland_theme.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 echo "source=./themes/$1.conf" > $HOME/.config/hypr/theme.conf #echo the theme conf file to the file hyprland will source
-
-#re-enable the vibrance shader
-sleep 2
-hyprshade on vibrance
