859bb88eec9edf0c8dbeb6e4d0323c585abae7e3
rome6315 <romo6315@gmail.com>
2024-12-30 22:10:10 -0500

    set cursor theme

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -12,9 +12,18 @@ aur_packages=$(aura -Qum)
 # get the package count for pacman in a different var. I wait a while to update my system so I dont want a whole list of 300+ packages in the tooltip
 pacman_packages=$(checkupdates 2>/dev/null | wc -l)
 
+# declare a variable that will show the proper icon unless there's no updates
+icon="󰇚"
+
+# want to see whats going to break my system next
 if [[ $updates_aur -eq 0 ]]; then
   aur_packages="No AUR updates!"
 fi
 
+# checkmark icon if no updates
+if [[ $updates -eq 0 ]]; then
+  icon=""
+fi
 
-echo "{\"text\": \"$updates\", \"tooltip\": \"$aur_packages\"}"
+
+echo "{\"text\": \"$icon $updates\", \"tooltip\": \"$aur_packages\"}"
